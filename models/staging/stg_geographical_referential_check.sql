9d91ce951102d73d2e1d8c55187da4bcef053288
josei.diaz@live.com <josei.diaz@live.com>
2023-08-28 15:57:25 +0000

    reviewed commitment.

--- models/staging/stg_geographical_referential_check.sql
+++ models/staging/stg_geographical_referential_check.sql
@@ -1,27 +1,10 @@
 {# geographical_referential table check #}
-
-{# Null values#}
+{# No null values or duplicates found#}
 SELECT
-    COUNT (municipality_code) IS NULL
-    ,COUNT (city_name) IS NULL
-    ,COUNT (city_name_normalized) IS NULL
-    ,COUNT (municipality_type) IS NULL
-    ,COUNT (latitude) IS NULL
-    ,COUNT (longitude) IS NULL
-    ,COUNT (department_name) IS NULL
-FROM
-    prello-lewagon.prello.geographical_referential
-
-{# Check number of Départments, 101 in total#}
-SELECT 
-    DISTINCT department_name
-FROM 
-    prello-lewagon.prello.geographical_referential
-
-{# Check duplicated rows#}
-SELECT 
-CONCAT(municipality_code, " ", latitude, " ", longitude ) as location
-, COUNT (*) AS nb
+    ,municipality_code
+    ,city_name_normalized AS city_name
+    ,municipality_type
+    ,CONCAT(municipality_code, " ", latitude, " ", longitude ) as location --used as primary key to check for duplicates
+    ,department_name
+    ,department_code
 FROM prello-lewagon.prello.geographical_referential
-GROUP BY location
-HAVING nb >1
