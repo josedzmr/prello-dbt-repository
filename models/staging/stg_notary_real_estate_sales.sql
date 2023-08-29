e41d6b1e9e06cbc4b8912e94a82c3fba5e926278
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-29 15:32:48 +0000

    NOTARY REAL ESTATE table
    Test no nulls outside of long&lat
    Removing most of outliers
    Removing """"duplicates""""
    Removing 97 department

--- models/staging/stg_notary_real_estate_sales.sql
+++ models/staging/stg_notary_real_estate_sales.sql
@@ -1,18 +1,25 @@
---test no nulls outside of long&lat / lots of duplicated? but is it really? / no <0 values
-
+/*
+Test no nulls outside of long&lat 
+Removing most of outliers
+Removing """"duplicates"""" 
+Removing 97 department 
+*/
+WITH test AS (
+SELECT  
+  sales_amount
+, sales_date
+, department
+, premise_type
+, SUM(number_of_principal_rooms) AS number_of_principal_rooms
+, SUM(surface) AS surface
+, SUM(sales_price_m2) AS sales_price_m2
+, COUNT(*) AS nb
+FROM `prello-lewagon.louis_test.stg_notary_partitioned` 
+WHERE department < '97'
+GROUP BY sales_amount, sales_date, department, premise_type
+HAVING nb = 1
+)
 SELECT 
-    DATE(sales_date) as sales_date -- Set as date
-    , sales_amount
-    , street_number
-    , street_code
-    , street_name
-    , nom_commune
-    , municipality_code
-    , LEFT(municipality_code,2) as department --french department are the two first numbers of the municipality code
-    , premise_type
-    , surface
-    , number_of_principal_rooms
-    , sales_price_m2	
-    , latitude
-    , longitude
-FROM `prello-lewagon.prello.notary_real_estate_sales` 
+    *
+FROM test 
+WHERE surface >= 10 AND sales_price_m2 BETWEEN 200 AND 80000 AND sales_amount > 20000
