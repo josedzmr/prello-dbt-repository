677b4e1c852b27e758ea2f1e299cbead18a43336
noesen.nicolas@gmail.com <noesen.nicolas@gmail.com>
2023-08-30 15:51:39 +0000

    stuff

--- models/staging/stg_frequentation_touristic_establishment.sql
+++ models/staging/stg_frequentation_touristic_establishment.sql
@@ -4,5 +4,5 @@ SELECT
   , hotel_overnight_stay_1000_ as overnight_stay_hotel
   , hotel_Foreigners___ as foreigners_hotel_rate
   , camping_overnight_stay_1000_ as overnight_stay_camping
-  , camping_Foreigners___ as fereigners_camping_rate
+  , camping_Foreigners___ as foreigners_camping_rate
 FROM `prello-lewagon.prello.frequentation_touristic_establishment`
