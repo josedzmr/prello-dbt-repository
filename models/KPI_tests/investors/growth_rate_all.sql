5b50ad8d4b09724bcb402d6325cee410b575a714
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-30 17:45:38 +0000

    Merging 'investors' KPI

---
+++ models/KPI_tests/investors/growth_rate_all.sql
@@ -0,0 +1,22 @@
+/*
+Merging 'investors' KPI
+Null values for dep. 57, 67 and 68 in:
+        rental_med_all
+        gross_yield
+        avg_m2_price_growth_last_year
+        sales_numbers_growth
+*/
+SELECT
+    pop.department
+,   pop.population_growth_last10y
+,   ROUND(eco.growth_rate_last5y,2) AS growth_rate_last5y
+,   yie.rental_med_all
+,   yie.gross_yield
+,   pri.avg_m2_price_growth_last_year
+,   pri.sales_numbers_growth
+FROM {{ ref('department_population_growth') }} pop
+LEFT JOIN {{ ref('economic_growth_rate') }} eco USING(department)
+LEFT JOIN {{ ref('gross_yield') }} yie USING(department)
+LEFT JOIN {{ ref('growth_volume_and_avg_price') }} pri USING(department)
+WHERE department < '97'
+ORDER BY department ASC
