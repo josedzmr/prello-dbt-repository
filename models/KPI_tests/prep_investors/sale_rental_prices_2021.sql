aa535df8aabbd7ea9d759c51bfc3867c91e7be13
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-30 14:32:42 +0000

    removing nested

--- models/KPI_tests/prep_investors/sale_rental_prices_2021.sql
+++ models/KPI_tests/prep_investors/sale_rental_prices_2021.sql
@@ -1,9 +1,4 @@
-SELECT 
-    sales.department
-    , ROUND(sales.avg_surface_2021, 2) as avg_surface_2021
-    , ROUND(sales.avg_sales_price_m2_2021, 2) as avg_sales_price_m2_2021
-    , ROUND(AVG(rental_prices.rental_med_all), 2) as rental_med_all
-FROM (
+WITH sales AS(
     SELECT 
         department
         , AVG(surface) as avg_surface_2021
@@ -12,8 +7,14 @@ FROM (
     WHERE EXTRACT(YEAR FROM DATE(sales_date)) = 2021
     GROUP BY department
     ORDER BY department
-) AS sales
-JOIN {{ ref('avg_rental_prices') }} AS rental_prices
+)
+SELECT 
+    sales.department
+    , ROUND(sales.avg_surface_2021, 2) as avg_surface_2021
+    , ROUND(sales.avg_sales_price_m2_2021, 2) as avg_sales_price_m2_2021
+    , ROUND(AVG(rental_prices.rental_med_all), 2) as rental_med_all
+FROM sales
+JOIN {{ ref("avg_rental_prices") }} AS rental_prices
 ON sales.department = rental_prices.department
 GROUP BY sales.department, sales.avg_surface_2021, sales.avg_sales_price_m2_2021
 ORDER BY sales.department
