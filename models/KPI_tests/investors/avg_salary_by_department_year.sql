3001cc9a717aa58e640c028f99a6232d09de906b
noesen.nicolas@gmail.com <noesen.nicolas@gmail.com>
2023-08-30 09:39:46 +0000

    links to staging & growth rate cleaning

--- models/KPI_tests/investors/avg_salary_by_department_year.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    LEFT(municipality_code,2) as department --french department are the two first numbers of the municipality code
-    , AVG(avg_net_salary) as avg_salary
-    , year
-FROM prello-lewagon.prello.average_salary_by_municipality
-GROUP BY department, year
-ORDER BY department, year;
--- models/KPI_tests/investors/economic_growth_rate.sql
+++ models/KPI_tests/investors/economic_growth_rate.sql
@@ -1,9 +1,17 @@
+WITH avg_year AS(
+    SELECT
+    LEFT(municipality_code,2) as department --french department are the two first numbers of the municipality code
+    , AVG(avg_net_salary) as avg_salary
+    , year
+FROM {{ref("stg_average_salary_by_municipality")}} 
+GROUP BY department, year
+ORDER BY department, year
+)
+
 SELECT
-    department,
-    year,
-    avg_salary,
-    (avg_salary - LAG(avg_salary) OVER (PARTITION BY department ORDER BY year)) / LAG(avg_salary) OVER (PARTITION BY department ORDER BY year) * 100 AS growth_rate
-FROM
-    prello-lewagon.dbt_mverstraete.avg_salary_by_department_year
-ORDER BY
-    department, year;
+    department
+    , year
+    , avg_salary
+    , (avg_salary - LAG(avg_salary) OVER (PARTITION BY department ORDER BY year)) / LAG(avg_salary) OVER (PARTITION BY department ORDER BY year) * 100 AS growth_rate
+FROM avg_year
+ORDER BY department, year
