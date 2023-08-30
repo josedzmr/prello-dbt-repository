2eee6b597f8e8e61cc666b641530641deb304449
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-30 06:43:52 +0000

    Adding some documentation

--- models/staging/stg_notary_real_estate_sales.sql
+++ models/staging/stg_notary_real_estate_sales.sql
@@ -1,9 +1,8 @@
 /*
 Creating a partioned table
-Test no nulls outside of long&lat 
 Removing most of outliers
 Removing """"duplicates"""" 
-Removing 97 department 
+Removing department '97' = overseas department
 */
 
 {{config(
@@ -18,25 +17,25 @@ Removing 97 department
 WITH create_department AS (
 SELECT
     *
-,   LEFT(municipality_code,2) AS department
+,   LEFT(municipality_code,2) AS department     -- create department column
 FROM `prello-lewagon.prello.notary_real_estate_sales`
 ),
 test AS (
 SELECT  
   sales_amount
-, DATE(sales_date) AS sales_date -- Set as date
+, DATE(sales_date) AS sales_date    -- Set as date
 , department
 , premise_type
-, SUM(number_of_principal_rooms) AS number_of_principal_rooms
-, SUM(surface) AS surface
-, SUM(sales_price_m2) AS sales_price_m2
+, SUM(number_of_principal_rooms) AS number_of_principal_rooms   -- easiest solution to show these columns due to 'groupby'
+, SUM(surface) AS surface   -- easiest solution to show these columns due to 'groupby'
+, SUM(sales_price_m2) AS sales_price_m2     -- easiest solution to show these columns due to 'groupby'
 , COUNT(*) AS nb
 FROM create_department 
-WHERE department < '97'
+WHERE department < '97'  -- remove overseas department
 GROUP BY sales_amount, sales_date, department, premise_type
-HAVING nb = 1
+HAVING nb = 1   -- keep rows that do not have the same 'sales_amount, sales_date, department, premise_type'
 )
 SELECT 
     *
 FROM test 
-WHERE surface >= 10 AND sales_price_m2 BETWEEN 200 AND 80000 AND sales_amount > 20000
+WHERE surface >= 10 AND sales_price_m2 BETWEEN 200 AND 80000 AND sales_amount > 20000   -- remove some outliers and wrong data, to be completed
