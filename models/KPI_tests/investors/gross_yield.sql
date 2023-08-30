04d4bd3b8b0c7461863ab337d6d3d7446f600e36
md verstraete <marie-desiree@verstraete.biz>
2023-08-30 14:11:39 +0000

    ss

--- models/KPI_tests/investors/gross_yield.sql
+++ models/KPI_tests/investors/gross_yield.sql
@@ -6,6 +6,6 @@ SELECT
     , avg_sales_price_m2_2021
     , ROUND(AVG(rental_med_all), 2) as rental_med_all
     , ROUND((AVG(rental_med_all) * 12) / (avg_sales_price_m2_2021 * avg_surface_2021) * 100,4) as gross_yield -- calculate the gross yield: first, multiplying the median rental price by 12 to approximate the annual rental income, then, divide by average investment
-FROM `prello-lewagon.dbt_mverstraete.sale_rental_prices_2021`
+FROM {{ ref('sale_rental_prices_2021') }}
 GROUP BY department, avg_surface_2021, avg_sales_price_m2_2021
 ORDER BY department
