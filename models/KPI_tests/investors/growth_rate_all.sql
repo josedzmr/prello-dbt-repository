53d63a13ef19fd3f5e799f0a53de4c188ac1a4c5
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-08-31 07:45:06 +0000

    add total columns to growth all

--- models/KPI_tests/investors/growth_rate_all.sql
+++ models/KPI_tests/investors/growth_rate_all.sql
@@ -8,11 +8,17 @@ Null values for dep. 57, 67 and 68 in:
 */
 SELECT
     pop.department
+,   pop.population_total
 ,   pop.population_growth_last10y
+,   eco.avg_salary
 ,   ROUND(eco.growth_rate_last5y,2) AS growth_rate_last5y
+,   yie.avg_sales_price_m2_2021
+,   yie.avg_surface_2021
 ,   yie.rental_med_all
 ,   yie.gross_yield
+,   pri.avg_m2_price
 ,   pri.avg_m2_price_growth_last_year
+,   pri.sales_numbers
 ,   pri.sales_numbers_growth
 FROM {{ ref('department_population_growth') }} pop
 LEFT JOIN {{ ref('economic_growth_rate') }} eco USING(department)
