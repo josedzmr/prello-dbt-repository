15321315bc0a23ed4e47522db04dcc0feb79e352
noesen.nicolas@gmail.com <noesen.nicolas@gmail.com>
2023-08-31 08:19:24 +0000

    Add of Sum for tourism

--- models/KPI_tests/tourisme/avg_importance_per_department.sql
+++ models/KPI_tests/tourisme/avg_importance_per_department.sql
@@ -2,6 +2,7 @@ WITH establishements AS (
   SELECT
   department
   , AVG(importance) AS avg_estab_importance
+  , SUM(importance) AS sum_estab_importance
 FROM {{ref("stg_POI_touristic_sites_by_municipality")}}
 GROUP BY department
 )
@@ -9,6 +10,7 @@ GROUP BY department
   SELECT
   department
   , AVG(importance) AS avg_site_importance
+  , SUM(importance) AS sum_site_importance
 FROM {{ref("stg_POI_tourist_establishments")}}
 GROUP BY department
 )
@@ -17,6 +19,7 @@ GROUP BY department
   SELECT
   department
   , AVG(poverty_rate) AS avg_poverty_rate
+  , SUM(poverty_rate) AS sum_poverty_rate
 FROM {{ref("stg_poverty_2018_insee")}}
 GROUP BY department
 )
@@ -24,8 +27,11 @@ GROUP BY department
 SELECT 
   site.department as department
   , site.avg_site_importance
+  , site.sum_site_importance
   , establishement.avg_estab_importance
+  , establishement.sum_estab_importance
   , poverty.avg_poverty_rate
+  , poverty.sum_poverty_rate
 FROM establishements as establishement
 INNER JOIN sites as site
 ON site.department = establishement.department
