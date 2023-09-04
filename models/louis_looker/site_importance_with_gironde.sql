7cabf9b5c35aa1b37916017e546a478b7b6e8dd6
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-09-04 15:45:49 +0000

    add ski import

--- models/louis_looker/site_importance_with_gironde.sql
+++ models/louis_looker/site_importance_with_gironde.sql
@@ -6,9 +6,21 @@ SELECT
         ELSE touristic_site
     END AS touristic_site_cleaned
 ,   CASE
-        WHEN department = '33' THEN importance
+        WHEN department = '06' THEN importance
         ELSE 0
-    END gir_import
+    END nice_import
+,   CASE
+        WHEN department = '34' THEN importance
+        ELSE 0
+    END montp_import
+,   CASE
+        WHEN department = '74' THEN importance
+        ELSE 0
+    END ski_import
+,   CASE
+        WHEN department = '75' THEN importance
+        ELSE 0
+    END paris_import
 --,   SUM(importance) OVER(PARTITION BY department, touristic_site) AS sum_importance_dep
 FROM {{ ref('stg_POI_touristic_sites_by_municipality') }}
 
@@ -16,7 +28,10 @@ FROM {{ ref('stg_POI_touristic_sites_by_municipality') }}
 SELECT
     department
 ,   touristic_site_cleaned
-,   SUM(gir_import) AS gir_sum_importance
+,   SUM(nice_import) AS nice_sum_importance
+,   SUM(montp_import) AS montp_sum_importance
+,   SUM(ski_import) AS ski_sum_importance
+,   SUM(paris_import) AS paris_sum_importance
 ,   SUM(importance) AS sum_importance
 FROM gironde
 GROUP BY department, touristic_site_cleaned
