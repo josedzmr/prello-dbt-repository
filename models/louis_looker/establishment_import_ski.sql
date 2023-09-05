f18547831bf5a21766e3e6434f8053a580863647
ls.mottet@gmail.com <ls.mottet@gmail.com>
2023-09-05 08:34:06 +0000

    upadted ski import with best municiplaity

--- models/louis_looker/establishment_import_ski.sql
+++ models/louis_looker/establishment_import_ski.sql
@@ -4,24 +4,19 @@ SELECT
 ,   CASE
         WHEN municipality_code = '74056' OR municipality_code = '74191' OR municipality_code = '74173' OR municipality_code = '74014' OR municipality_code = '74236' OR municipality_code = '74136' OR municipality_code = '74063' OR municipality_code = '74080' OR municipality_code = '74258' OR municipality_code = '74134' OR municipality_code = '74085' OR municipality_code = '74083' OR municipality_code = '74215' OR municipality_code = '74190' OR municipality_code = '74160' OR municipality_code = '74238' OR municipality_code = '74279' OR municipality_code = '74058' OR municipality_code = '74099' THEN importance
         ELSE 0
-    END best_ski_import
---,   CASE
---        WHEN municipality_code = '74056' OR municipality_code = '74191' OR municipality_code = '74173' OR municipality_code = '74014' OR municipality_code = '74236' OR municipality_code = '74136' OR municipality_code = '74063' OR municipality_code = '74080' OR municipality_code = '74258' OR municipality_code = '74134' OR municipality_code = '74085' OR municipality_code = '74083' OR municipality_code = '74215' OR municipality_code = '74190' OR municipality_code = '74160' OR municipality_code = '74238' OR municipality_code = '74279' OR municipality_code = '74058' OR municipality_code = '74099' OR THEN '100'
---        ELSE department
---    END best_department
+    END AS best_savoie_esta_import
+,   CASE
+        WHEN department = '74' THEN importance
+        ELSE 0
+    END AS savoie_esta_import
 --,   SUM(importance) OVER(PARTITION BY department, touristic_site) AS sum_importance_dep
 FROM {{ ref('stg_POI_tourist_establishments') }}
 )
-,   prep_join AS(
 SELECT
     department
 ,   accomodation_type
-,   SUM(best_ski_import) AS best_ski_sum_importance
-,   SUM(importance) AS sum_esta_importance
+,   SUM(best_savoie_esta_import) AS best_esta_importance
+,   SUM(savoie_esta_import) AS savoie_esta_importance
+,   SUM(importance) AS total_esta_importance
 FROM esta
 GROUP BY department, accomodation_type
-)
-SELECT
-    *
-,   SUM(sum_esta_importance) OVER() AS esta_importance_total
-FROM prep_join
--- models/louis_looker/site_import_ski.sql
+++ models/louis_looker/site_import_ski.sql
@@ -2,24 +2,21 @@ WITH site AS (
 SELECT
     *
 ,   CASE
+        WHEN municipality_code = '74056' OR municipality_code = '74191' OR municipality_code = '74173' OR municipality_code = '74014' OR municipality_code = '74236' OR municipality_code = '74136' OR municipality_code = '74063' OR municipality_code = '74080' OR municipality_code = '74258' OR municipality_code = '74134' OR municipality_code = '74085' OR municipality_code = '74083' OR municipality_code = '74215' OR municipality_code = '74190' OR municipality_code = '74160' OR municipality_code = '74238' OR municipality_code = '74279' OR municipality_code = '74058' OR municipality_code = '74099' THEN importance
+        ELSE 0
+    END AS best_savoie_site_import
+,   CASE
         WHEN department = '74' THEN importance
         ELSE 0
-    END ski_site_import
-
+    END AS savoie_site_import
 --,   SUM(importance) OVER(PARTITION BY department, touristic_site) AS sum_importance_dep
 FROM {{ ref('stg_POI_touristic_sites_by_municipality') }}
-
 )
-,   prep_join AS(
 SELECT
     department
 ,   touristic_site
-,   SUM(ski_site_import) AS ski_sum_importance
-,   SUM(importance) AS sum_site_importance
+,   SUM(best_savoie_site_import) AS best_site_importance
+,   SUM(savoie_site_import) AS savoie_site_importance
+,   SUM(importance) AS total_site_importance
 FROM site
 GROUP BY department, touristic_site
-)
-SELECT
-    *
-,   SUM(sum_site_importance) OVER() AS site_importance_total
-FROM prep_join
